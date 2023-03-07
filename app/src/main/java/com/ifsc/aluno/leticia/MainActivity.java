47f8304d4e4f0ba97a51a6d318c3ce9ba322f29d
LeleMaria1 <leticiaonofre01@gmail.com>
2023-03-07 17:21:06 -0300

    welcomeToast

--- app/src/main/java/com/ifsc/aluno/leticia/MainActivity.java
+++ app/src/main/java/com/ifsc/aluno/leticia/MainActivity.java
@@ -2,7 +2,11 @@ package com.ifsc.aluno.leticia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button welcomeButton = findViewById(R.id.welcomeButton);
+        welcomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //inicializa as informações necessárias para criar uma mensagem Toast
+                Context contexto = MainActivity.this;
+                CharSequence mensagem = getResources().getString(R.string.welcomeToast);
+                int duracao = Toast.LENGTH_SHORT;
+                //cria uma mensagem Toast com as informações acima
+                Toast myToast = Toast.makeText(contexto, mensagem, duracao);
+                //manda mostrar a mensagem Toast
+                myToast.show();
+            }
+        });
     }
 }
